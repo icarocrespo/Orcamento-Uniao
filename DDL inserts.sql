2d5aa13614f1b478ac095990db9a5f6dcf973497
Ícaro Crespo <icarocrespo@gmail.com>
2022-01-13 12:28:26 -0300

    Upload dos demais arquivos gerados até o momento

---
+++ DDL inserts.sql
@@ -0,0 +1,24 @@
+-- Inserções 10 primeiros registros em orgao_superior
+INSERT INTO ORGAO_SUPERIOR(ID_ORGAO_SUPERIOR, NOME_ORGAO_SUPERIOR, EXERCICIO)
+	VALUES (20000, 'Presidência da República', 2021);
+
+
+-- Inserções 10 primeiros registros em orgao_subordinado	
+INSERT INTO ORGAO_SUBORDINADO(ID_ORGAO_SUBORDINADO, NOME_ORGAO_SUBORDINADO, ID_ORGAO_SUPERIOR)
+	VALUES(20101, 'PRESIDENCIA DA REPUBLICA', 20000);
+	
+-- Inserções 10 primeiros registros em unidade_orcamentaria
+
+-- Inserções 10 primeiros registros em funcao
+
+-- Inserções 10 primeiros registros em subfuncao
+
+-- Inserções 10 primeiros registros em programa_orcamentario
+
+-- Inserções 10 primeiros registros em acao
+
+-- Inserções 10 primeiros registros em categoria_economica
+
+-- Inserções 10 primeiros registros em grupo_despesa
+
+-- Inserções 10 primeiros registros em elemento_despesa
